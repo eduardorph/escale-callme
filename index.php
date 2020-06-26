8ecacd95f4bdda6e0528e808844fc6f693429699
Eduardo Freitas <eduardorph@hotmail.com>
2020-06-26 14:19:58 -0300

    Troca do input por button

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ defined( 'ABSPATH' ) or die();
 /*
 * Plugin name: Escale Callme
 * Description: Plugin para integrar ao callback da Claro
-* Version: 1.2.1
+* Version: 1.2.2
 * Author: Eduardo - Proteína Digital
 */
 define('ESCALE_CALLME_PLUGIN_FILE_URL', __FILE__);
@@ -28,13 +28,13 @@ if (!empty($options["callme_api_url"]) && !empty($options["callme_api_key"]) && 
             wp_enqueue_script( 'jquery' );
         }
 
-        wp_register_style( 'escale_callme', plugins_url('/css/escale_callme.css', __FILE__), false, '1.0.4', 'all');
+        wp_register_style( 'escale_callme', plugins_url('/css/escale_callme.css', __FILE__), false, '1.0.5', 'all');
         wp_enqueue_style( 'escale_callme' );
 
         wp_register_script( 'escale_callme_mask', plugins_url('/lib/mask/jquery.mask.min.js',__FILE__ ), false, '1.0.0', 'all');
         wp_enqueue_script('escale_callme_mask');
 
-        wp_register_script( 'escale_callme', plugins_url('/js/escale_callme.js',__FILE__ ), false, '1.0.10', 'all');
+        wp_register_script( 'escale_callme', plugins_url('/js/escale_callme.js',__FILE__ ), false, '1.0.11', 'all');
         wp_enqueue_script('escale_callme');
 
         wp_localize_script('escale_callme', 'ajax_object_callme', array('ajax_url_callme' => admin_url('admin-ajax.php'), 'outro_valor' => 102025, 'ajax_nonce' => wp_create_nonce('escale_callme_3546245624')));
